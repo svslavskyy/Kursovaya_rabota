7e752a490cdb3a17c86664ee8d12f967b4a70fae
Slavskyy Svyatoslav <svslavskyy@gmail.com>
2019-04-05 11:55:42 +0300

    Start

---
+++ Main.cpp
@@ -0,0 +1,26 @@
+/* ----------------------------------------------------------------<Header>-
+ Name: Main.cpp
+ Group: TI-81
+ Students: Slavskyy S.V., Dubrov B.Y.
+ Written: 
+ Revised: 
+ Description:. 
+    
+ ------------------------------------------------------------------</Header>-*/
+#include <iostream>
+using namespace std;
+class samp {
+ 	int a;
+public:
+ 	samp(int i){a=i;}
+ 	samp(){};
+ 	int get_a() { return a; }
+ 	void put(int i){a=i;}
+};
+int main() {
+ 	
+}
+
+/* ---------------------------------------------------------------------[<]-
+Test1 output:
+---------------------------------------------------------------------[>]-*/
